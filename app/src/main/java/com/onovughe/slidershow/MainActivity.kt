e32d57cc6e1f4d7e0170df5f4098e65d3951ddc9
akpor26 <onovughe26@gmail.com>
2021-08-10 00:21:32 +0100

    Slider

--- app/src/main/java/com/onovughe/slidershow/MainActivity.kt
+++ app/src/main/java/com/onovughe/slidershow/MainActivity.kt
@@ -23,8 +23,8 @@ class MainActivity : AppCompatActivity() {
 
         imageList.add(SlideModel("https://images.daznservices.com/di/library/GOAL/5d/42/kelechi-iheanacho-leicester-community-shield_n301ua443ijs19iv8xem0wi7j.jpg?quality=60&w=700", "Kelechi"))
         imageList.add(SlideModel("https://images.daznservices.com/di/library/GOAL/29/25/lionel-messi-press-conference_1ols8hmny27781e1egfocee3nd.jpg?quality=60&w=700", "Messi"))
-        imageList.add(SlideModel("https://dailypost.ng/wp-content/uploads/2021/08/ronaldo.jpeg", "Ronaldo"))
-        imageList.add(SlideModel("https://dailypost.ng/wp-content/uploads/2020/11/Kylian-Mbappe.jpg.webp", "Kylian"))
+        imageList.add(SlideModel("https://372673.smushcdn.com/318461/wp-content/uploads/2021/08/Lie-artwork-768x768.jpeg?lossy=1&strip=1&webp=1", "Kizz Daniel"))
+        imageList.add(SlideModel("https://372673.smushcdn.com/318461/wp-content/uploads/2021/08/IMG-20210722-WA0024-scaled.jpg?lossy=1&strip=1&webp=1", "Yemisi"))
 
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
     }
